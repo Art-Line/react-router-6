fdb8978c58d9a6436a3d1e7eb2bc406be30f56f0
Art_Line <serg20004@ukr.net>
2023-03-21 15:34:27 +0100

    custom link component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,22 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import CustomLink from './CustomLink';
+
+// const setActive = ({isActive}) => isActive ? 'active-class' : '';
 
 function Layout() {
 	return (
 		<>
 			<header className="header">
 				<div className="container">
+					{/* <ul className="main-menu">
+						<li><NavLink className={setActive} to="/">Home</NavLink></li>
+						<li><NavLink className={setActive} to="/posts">Posts</NavLink></li>
+						<li><NavLink className={setActive} to="/contact">Contact</NavLink></li>
+					</ul> */}
 					<ul className="main-menu">
-						<li><Link to="/">Home</Link></li>
-						<li><Link to="/posts">Posts</Link></li>
-						<li><Link to="/contact">Contact</Link></li>
+						<li><CustomLink to="/">Home</CustomLink></li>
+						<li><CustomLink to="/posts">Posts</CustomLink></li>
+						<li><CustomLink to="/contact">Contact</CustomLink></li>
 					</ul>
 				</div>
 			</header>
